617280b9ed84650099dd416afd7b45fde52824cc
loque18 <madridx2010@gmail.com>
2023-01-27 11:33:02 -0500

    feat: added web

---
+++ web/src/app/layout.jsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { useState } from 'react';
+import 'styles/bulma.css';
+
+const themes = {
+	dark: 'dark',
+	light: 'light',
+};
+
+export default function RootLayout({ children }) {
+	const [theme, setTheme] = useState(themes.light);
+
+	const toggleTheme = () => {
+		setTheme(theme === themes.dark ? themes.light : themes.dark);
+	};
+
+	return (
+		<html lang="en">
+			{/*
+        <head /> will contain the components returned by the nearest parent
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+			<head />
+			<body className={theme}>
+				<button className="button" onClick={toggleTheme}>
+					<span className="icon">
+						{theme === themes.dark ? (
+							<i className="fad fa-sun" />
+						) : (
+							<i className="fad fa-moon" />
+						)}
+					</span>
+				</button>
+				{children}
+			</body>
+		</html>
+	);
+}
---
+++ web/src/app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<section>
+			<div className="container">
+				<div className="box2">
+					<h1>Home</h1>
+				</div>
+			</div>
+		</section>
+	);
+}
